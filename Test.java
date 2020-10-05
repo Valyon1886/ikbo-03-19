b009ae5079c636782d52c2aa6c8805bcd7d4e91c
Valyon1886 <64031088+Valyon1886@users.noreply.github.com>
2020-10-05 22:16:11 +0300

    Лабоаторная работа №7

--- Test.java
+++ Test.java
@@ -1,20 +1,39 @@
-package Pack2;
-
+package Pack1;
+import java.util.ArrayList;
+//я добавил недостающие методы
 public class Test {
     public static void main(String[] args) {
-        MovablePoint dot = new MovablePoint(0, 0, 13, 13);
-        System.out.println(dot);
-        dot.moveDown();
-        System.out.println(dot);
-        MovableCircle okr = new MovableCircle(0,0, 14, 14, 5);
-        System.out.println(okr);
-        okr.moveUp();
-        System.out.println(okr);
-        MovableRectangle sqr = new MovableRectangle(0, 0, 12, 6, 5, 6);
-        System.out.println(sqr);
-        sqr.moveRight();
-        System.out.println(sqr);
-        sqr.moveLeft();
-        System.out.println(sqr);
+        ArrayList<String> states1 = new ArrayList<String>();
+        ArrayList<String> states2 = new ArrayList<String>(20);
+// добавим в список ряд элементов
+        states1.add("Германия");
+        states1.add("Франция");
+        states1.add("Великобритания");
+        states1.add("Испания");
+        states1.add("Германия");
+        states1.add(1, "Италия"); // добавляем элемент по индексу 1
+
+        states2.addAll(0, states1); //добавляю все элементы из states1 в stetes2
+        System.out.println(states2.indexOf("Германия")); //первый индекс элемента = "Германия"
+
+
+        System.out.println(states1.get(1));// получаем 2-й объект
+        states1.set(1, "Дания"); // установка нового значения для 2-го объекта
+        System.out.printf("В списке %d элементов \n", states1.size());
+        for(String state : states1){
+            System.out.println(state);
+        }
+        if(states1.contains("Германия")){
+            System.out.println("Список содержит государство Германия");
+        }
+
+// удалим несколько объектов
+        states1.remove("Германия");
+        states1.remove(0);
+        Object[] countries = states1.toArray();
+        for(Object country : countries){
+            System.out.println(country);
+        }
     }
 }
+
